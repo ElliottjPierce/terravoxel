7a693c91f5e01faf5f72bac2d56fe015c0086e05
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-05-15 13:19:34 -0400

    workflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![allow(
+    clippy::doc_markdown,
+    reason = "These rules should not apply to the readme."
+)]
+#![doc = include_str!("../README.md")]
+
+/// Doc comment for CI
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
