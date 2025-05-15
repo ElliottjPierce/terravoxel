dc437be3fbabb393405b5198d199c15d204e092e
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-05-15 13:25:57 -0400

    clean slate

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,3 @@
     reason = "These rules should not apply to the readme."
 )]
 #![doc = include_str!("../README.md")]
-
-/// Doc comment for CI
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
