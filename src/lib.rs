97268ae6de7266665659c9378c76a993a783d84f
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-05-21 23:18:24 -0400

    feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
     clippy::doc_markdown,
     reason = "These rules should not apply to the readme."
 )]
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![doc = include_str!("../README.md")]
 
 pub mod storage;
